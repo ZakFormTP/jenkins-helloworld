b43ee690c4cfc4950dee81a16105232be83e5371
ZakFormTP <boualiazak@gmail.com>
2026-07-07 09:13:34 +0200

    Update Main.java
    cours pipeline

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Zak! Ceci est le test du premier trigger Git!");
+        System.out.println("Hello, Zak!!!");
     }
 }
 
